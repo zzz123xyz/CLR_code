5682a7910c2859f92dd7f1275f70e36679b213bc
Lance Liu <zyx321zzz@gmail.com>
2016-05-15 22:35:39 +1000

    InitialGraphCLR works

--- InitialGraphCLR.m
+++ InitialGraphCLR.m
@@ -7,15 +7,23 @@ function A = InitialGraphCLR(data, m)
 n = size(data,2);
 E = pdist2(data', data').^2;
 
-[SortedE, Ind] = sort(E); 
-
-SortedE = SortedE'; Ind = Ind';
+[SortedE, Ind] = sort(E); SortedE = SortedE'; Ind = Ind';
 SortedE = [SortedE(:,2:end),SortedE(:,1)]; Ind = [Ind(:,2:end),Ind(:,1)];
+Eim1 = repmat(SortedE(:,m+1),[1,n]); 
+Eim1Ind = Ind(:,m+1);
 
-Eim1 = repmat(SortedE(:,m+1),[1,n]);
+%*****
+%make a Indicator Mask to record j<=m or j>m
+IndMask = zeros(n); 
+for i = 1:n
+    IndMask(i, Ind(i,1:m)) = 1;
+end
+
+E_numerator = E.*IndMask;
+Eim1_numerator = Eim1.*IndMask;
+
 denominator = m*Eim1 - repmat(sum(SortedE(:,1:m),2),[1,n]);
-A = (Eim1 - SortedE)./denominator;
+A = (Eim1_numerator - E_numerator)./denominator;
 
-% j>m aij = 0;
-A(:,m+1:end) = 0;
+
     
